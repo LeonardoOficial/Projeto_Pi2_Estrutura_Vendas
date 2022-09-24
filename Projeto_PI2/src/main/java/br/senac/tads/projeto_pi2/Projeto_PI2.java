63b450fd105e71da4ce82115f66cad629a5f49e5
Leonardo <Leonardo@DESKTOP-GJQD0L8.Home>
2022-09-24 18:55:40 -0300

    Tela de cliente

---
+++ Projeto_PI2/src/main/java/br/senac/tads/projeto_pi2/Projeto_PI2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package br.senac.tads.projeto_pi2;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Projeto_PI2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
